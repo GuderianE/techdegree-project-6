83ae61c484141577823c2a8a57eee8beee9c7867
Elias Guderian <46761652+GuderianE@users.noreply.github.com>
2019-06-03 00:48:10 +0200

    added addPhraseToDisplay
    function that dispalys a phrase from a randomised array

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const startBtn = document.querySelector('.btn__reset');
 const overlay = document.querySelector('#overlay');
 const mainDiv = document.querySelector('.main-container');
 
+// removes overlay
 startBtn.addEventListener('click', (e) => {
     mainDiv.removeChild(overlay);
 });
@@ -16,12 +17,25 @@ const phrases =
 'cannot touch this', 
 'We are the champions my friend'];
 
+// selects random phrase and spilts it into letters
 const getRandomPhraseAsArray = (arr) => {
-    let randomSelector = arr[Math.floor(Math.random() * arr.length)];
-    let  random = randomSelector.split('');
+    const randomSelector = arr[Math.floor(Math.random() * arr.length)];
+    const  random = randomSelector.split('');
     return random;
 };
 
+const addPhraseToDisplay = (arr) => {
+    for (let i = 0; i < arr.length; i++){
+        const listItem = document.createElement('li');
+        listItem.textContent = arr[i];
+        const ul = phrase.firstElementChild;
+        ul.appendChild(listItem);
+        if (arr[i] !== ' ') {
+            listItem.className = 'letter';
+        }
+    }
+};
 
 
 const phraseArray = getRandomPhraseAsArray(phrases);
+addPhraseToDisplay(phraseArray);
