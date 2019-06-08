a6751ec0f2f009cccd0a09fe08f304234a6bba63
Elias Guderian <46761652+GuderianE@users.noreply.github.com>
2019-06-08 15:23:23 +0200

    sone changes to letterFound if statement

--- app.js
+++ app.js
@@ -63,15 +63,11 @@ qwerty.addEventListener('click', (e) => {
         checkLetter(e.target); 
     }
     const letterFound = checkLetter(e.target);
-    if (letterFound === '') {
+    if (letterFound === null) {
         missed += 1;
-        console.log(missed);
+        console.log('missed');
         const tries = document.querySelectorAll('.tries');
-        const img = document.querySelectorAll('img');
-        for (let i = 0; i < tries.length; i++) {
-            
-            tries[i].removeChild(img[i]);
-        }
+            tries[missed].style.display = 'none';
     }
 });
 
